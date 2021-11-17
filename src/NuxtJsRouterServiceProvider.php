c84df2f42efbfd3975e0894b5daad9c8941a4ee6
VMaarsson <viktor@maarsson.se>
2021-11-17 07:31:53 +0100

    Create package service provider

---
+++ src/NuxtJsRouterServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Maarsson\NuxtJsRouter;
+
+use Illuminate\Support\ServiceProvider;
+
+class NuxtJsRouterServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any package services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
