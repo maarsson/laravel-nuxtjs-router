7f6a096866c35a2d7e553ecf4408d88fb8ecef4d
VMaarsson <viktor@maarsson.se>
2022-03-09 07:03:14 +0100

    Refactor

--- src/Http/Controllers/RouteController.php
+++ src/Http/Controllers/RouteController.php
@@ -7,42 +7,37 @@ use Illuminate\Routing\Controller;
 class RouteController extends Controller
 {
     /**
-     * Get the NuxtJS view.
+     * Handles the NuxtJS file request.
      *
      * @return \Illuminate\Http\Response
      */
     public function __invoke()
     {
-        $folder = realpath(public_path(config('nuxtjs.public_folder')));
+        $nuxtPublicFolderPath = realpath(public_path(config('nuxtjs.public_folder')));
+        $nuxtRequestPath = $nuxtPublicFolderPath . request()->getRequestUri();
+        $nuxtIndexPath = $nuxtPublicFolderPath . '/index.html';
+        $filePath = file_exists($nuxtRequestPath) && ! is_dir($nuxtRequestPath) ? $nuxtRequestPath : $nuxtIndexPath;
 
         if (
-            $folder === false
-            || ! is_dir($folder))
+            $nuxtPublicFolderPath === false
+            || ! is_dir($nuxtPublicFolderPath))
         {
-            // NuxtJs public path does not exists
-            abort(412, 'The frontend was not compiled yet');
+            abort(412, 'The NuxtJS public path does not exists! (Frontend was not compiled yet?)');
         }
 
-        $path = $folder . request()->getRequestUri();
-
-        if (
-            file_exists($path)
-            && ! is_dir($path)
-        ) {
-            // The request was an existing file to be loaded
-            include $path;
-
-            return;
-        }
-
-        $indexPath = $folder . '/index.html';
-
-        if (! file_exists($indexPath))
+        if (! file_exists($nuxtIndexPath))
         {
-            // NuxtJs index file does not exists
-            abort(422, 'The frontend loader not exists');
+            abort(422, 'The NuxtJS index file does not exists!');
         }
 
-        return file_get_contents($indexPath);
+        if (! file_exists($filePath))
+        {
+            abort(422, 'The requested file does not exists!');
+        }
+
+        return response(
+            file_get_contents($filePath),
+            200,
+        );
     }
 }
